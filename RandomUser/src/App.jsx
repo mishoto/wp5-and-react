05d953df4b967ec3ca749000915ef85b54995826
mishoto <mihail.dev.io@gmail.com>
2022-07-30 11:06:36 +0300

    update/RP-1.0.0- add implementation logic

--- RandomUser/src/App.jsx
+++ RandomUser/src/App.jsx
@@ -13,11 +13,109 @@ const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg';
 
 import './index.css';
 
-const App = () => (
-  <>
-    <h3>random user</h3>
-  </>
-);
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [title, setTitle] = useState('name');
+  const [value, setValue] = useState('random person');
+
+  const getUser = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const user = data.results[0];
+    const { phone, email } = user;
+    const { large: image } = user.picture;
+    const {
+      login: { password },
+    } = user;
+    const { first, last } = user.name;
+    const {
+      dob: { age },
+    } = user;
+    const {
+      street: { number, name },
+    } = user.location;
+    const newUser = {
+      image,
+      phone,
+      email,
+      password,
+      age,
+      street: `${number} ${name}`,
+      name: `${first} ${last}`,
+    };
+
+    setUser(newUser);
+    setLoading(false);
+    setTitle('name');
+    setValue(newUser.name);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const handleValue = (e) => {
+    if (e.target.classList.contains('icon')) {
+      const newValue = e.target.dataset.label;
+      setValue(user[newValue]);
+      setTitle(newValue);
+    }
+  };
+
+  return (
+    <main>
+      <div className='block bcg-black'></div>
+      <div className='block'>
+        <div className='container'>
+          <img
+            src={(user && user.image) || defaultImage}
+            alt='randomUser'
+            className='user-img'
+          />
+          <p className='user-title'>my {title} is</p>
+          <p className='user-value'>{value}</p>
+          <div className='values-list'>
+            <button
+              className='icon'
+              data-label='name'
+              onMouseOver={handleValue}>
+              <FaUser />
+            </button>
+            <button
+              className='icon'
+              data-label='email'
+              onMouseOver={handleValue}>
+              <FaEnvelopeOpen />
+            </button>
+            <button className='icon' data-label='age' onMouseOver={handleValue}>
+              <FaCalendarTimes />
+            </button>
+            <button
+              className='icon'
+              data-label='street'
+              onMouseOver={handleValue}>
+              <FaMap />
+            </button>
+            <button
+              className='icon'
+              data-label='phone'
+              onMouseOver={handleValue}>
+              <FaPhone />
+            </button>
+            <button
+              className='icon'
+              data-label='password'
+              onMouseOver={handleValue}>
+              <FaLock />
+            </button>
+          </div>
+          <button className='btn' type='button' onClick={getUser}>
+            {loading ? 'loading...' : 'random user'}
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+};
 ReactDOM.render(<App />, document.getElementById('app'));
-
-
