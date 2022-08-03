6228cf9485ad20a531f2f18064c438ab087d3288
mishoto <mihail.dev.io@gmail.com>
2022-08-03 19:43:47 +0300

    update/SP-1.0.0- add .env vars & data fetching

--- StockPhotos/src/components/PhotosList.jsx
+++ StockPhotos/src/components/PhotosList.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import Photo from './Photo'
+import React, { useEffect, useState } from 'react';
+import Photo from './Photo';
+import { FaSearch } from 'react-icons';
 
-const mainUrl = `https://api.unsplash.com/photos/`;
+
+const mainUrl = `https://api.unsplash.com/photos/?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 const PhotosList = () => {
-  return (
-    <div>PhotosList</div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const[photos, setPhotos] = useState([]);
 
-export default PhotosList
+  const fetchImages = async () => {
+    let url;
+    url = `${mainUrl}`;
+    try{
+      const response = await fetch(url);
+      const responseJson = await response.json();
+
+    }catch(error){
+      setLoading(false);
+      console.log(error);
+    }
+    
+  };
+
+  useEffect(() => {
+    
+    fetchImages();
+  }, []);
+
+  return <div>PhotosList</div>;
+};
+
+export default PhotosList;
