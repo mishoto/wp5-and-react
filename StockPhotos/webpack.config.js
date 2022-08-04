e17af3f858787ac1178a5787a1a7ceb3c3cb2a75
mishoto <mihail.dev.io@gmail.com>
2022-08-04 12:59:39 +0300

    update/SP-1.0.0- add entry attr & Dotenv plugin

--- StockPhotos/webpack.config.js
+++ StockPhotos/webpack.config.js
@@ -1,71 +1,83 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  output: {
-    publicPath: 'http://localhost:3006/',
-
-    path: path.resolve(__dirname, './dist'),
-    assetModuleFilename: 'images/[hash][ext][query]',
-  },
-
-  resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
-  },
-
-  devServer: {
-    port: 3006,
-    historyApiFallback: true,
-    hot: true,
-
-    static: {
-      directory: path.join(__dirname, './dist'),
+module.exports = (env) => {
+  return {
+    entry: {
+      app: [path.join(__dirname, './src/App.jsx')],
     },
-  },
 
-  devtool: 'source-map',
+    output: {
+      publicPath: 'http://localhost:3006/',
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: 'javascript/auto',
-        resolve: {
-          fullySpecified: false,
-        },
+      path: path.resolve(__dirname, './dist'),
+      assetModuleFilename: 'images/[hash][ext][query]',
+    },
+
+    resolve: {
+      extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+    },
+
+    devServer: {
+      port: 3006,
+      historyApiFallback: true,
+      hot: true,
+
+      static: {
+        directory: path.join(__dirname, './dist'),
       },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg)$/i,
-        type: 'asset/resource',
-        use: [
-          'file-loader',
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-              mimetype: ['image/png', 'image/jpg', 'image/gif'],
-              encoding: true,
-            },
+    },
+
+    devtool: 'source-map',
+
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: 'javascript/auto',
+          resolve: {
+            fullySpecified: false,
           },
-        ],
-      },
-    ],
-  },
+        },
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ['style-loader', 'css-loader', 'postcss-loader'],
+        },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/i,
+          type: 'asset/resource',
+          use: [
+            'file-loader',
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+                mimetype: ['image/png', 'image/jpg', 'image/gif'],
+                encoding: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
 
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-    }),
-  ],
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+      }),
+
+      new Dotenv({
+        
+        path: path.resolve(__dirname, './environments/.env.dev'),
+      }),
+    ],
+  };
 };
